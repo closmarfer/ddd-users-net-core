9df9cd1568741e39c1fd7041c52b762d2bfc96bf
closmarfer <carlos@closmarfer.com>
2020-06-03 07:32:32 +0200

    First approach to Solution structure

---
+++ Users.Domain/ValueObject/Email.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Users.Domain.ValueObject
+{
+    public class Email
+    {
+        public String Value { get; private set; }
+
+        public Email(String value)
+        {
+            Value = value;
+        }
+    }
+}
