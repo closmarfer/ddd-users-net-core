0f0955876ba65b32058e9822eca6cace15a0c6a4
closmarfer <carlos@closmarfer.com>
2020-06-07 08:12:59 +0200

    launchSettings updated. TODO added to MySqlProvider.cs

--- Users.Infrastructure/Repository/MySQL/MySqlProvider.cs
+++ Users.Infrastructure/Repository/MySQL/MySqlProvider.cs
@@ -1,10 +1,4 @@
-﻿using Dapper;
-using System;
-using System.Data;
-using System.Data.Common;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿using System.Data.Common;
 
 namespace Users.Infrastructure.Repository.MySQL
 {
@@ -12,7 +6,9 @@ namespace Users.Infrastructure.Repository.MySQL
     {
 
         public DbProviderFactory Factory => MySql.Data.MySqlClient.MySqlClientFactory.Instance;
-        public string GetConnectionString() => "Server=db;Port=3306;Database=users;Uid=root;Pwd=root;";
+        
+        //TODO load from environment
+        public string GetConnectionString() => "Server=db;Port=3306;Database=users;Uid=root;Pwd=root;"; 
 
         public DbConnection GetMySqlConnection(bool open = true,
             bool convertZeroDatetime = false, bool allowZeroDatetime = false)
